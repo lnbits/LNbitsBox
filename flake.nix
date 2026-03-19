6055f9561bc708150282d818ae629f4bcea5367e
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2026-03-19 10:02:51 +0000

    Bump flake version

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
 
   outputs = { self, nixpkgs, raspberry-pi-nix, lnbits, spark-sidecar, ... }:
   let
-    version = "0.9.7";  # Bump before each release tag to match the next tag name
+    version = "0.9.8";  # Bump before each release tag to match the next tag name
     system = "aarch64-linux";
   in
   {
