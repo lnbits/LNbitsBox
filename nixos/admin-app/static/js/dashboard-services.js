51ca32ced310383c958b2c5f7edcc391105e0a9e
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2026-06-11 13:07:46 +0200

    Use replaceAll in dashboard HTML escaping

--- nixos/admin-app/static/js/dashboard-services.js
+++ nixos/admin-app/static/js/dashboard-services.js
@@ -2,11 +2,11 @@
     const D = window.LNbitsBoxDashboard;
     const escapeHtml = function (value) {
         return String(value || '')
-            .replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;');
+            .replaceAll('&', '&amp;')
+            .replaceAll('<', '&lt;')
+            .replaceAll('>', '&gt;')
+            .replaceAll('"', '&quot;')
+            .replaceAll("'", '&#39;');
     };
     D.escapeHtml = escapeHtml;
 
